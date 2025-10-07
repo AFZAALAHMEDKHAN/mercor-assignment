496a4bb5045183aa95d87e0bfa56052dedc215cd
AFZAALAHMEDKHAN <afzaalahmedk861@gmail.com>
2025-10-07 19:33:12 +0530

    updating with backend and workflow

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mercor-ecs-bluegreen-tfstate" 
-    key    = "environments/ecs-bluegreen/terraform.tfstate" 
-    region = "us-east-1"
+    bucket  = "mercor-ecs-bluegreen-tfstate"
+    key     = "environments/ecs-bluegreen/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
